8bbd57d0c17d4f93bee833486739f5cff59e1fc9
Pratyusha <um20216@stu.ximb.ac.in>
2020-10-09 14:21:16 +0530

    Create XIM3.py

---
+++ XIM3.py
@@ -0,0 +1,98 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Sep  6 16:54:05 2020
+
+@author: vikas
+"""
+
+#Conditions
+
+#if
+
+'''
+if condition:
+    statements
+
+'''
+
+a=9
+
+if (a<10):
+    print("a is less than 10")
+
+
+a=9
+b=10
+
+if(a<b):
+    print("a is lesser than b")
+
+a
+b
+
+a==b
+a<b
+a>b
+a<=b
+a>=b
+a!=b
+
+
+x=2
+if (x==1):
+    print("Right")
+
+if(x==1):
+    print("Right")
+else:
+    print("Wrong")
+
+
+x=30
+y=20
+
+if(x>y):
+    print("X is greater")
+else:
+    print("Y is greater")
+    
+    
+    
+Marks=85
+
+if (Marks>80):
+    print("A")
+elif (Marks>70 and Marks<=80):
+    print("B")
+elif (Marks>60 and Marks<=70):
+    print("C")
+else:
+    print("Fail")
+    
+
+a=10
+b=40
+c=30
+
+a>b or b>c
+
+
+#Loops
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
